6873eb06072ef87398c1ae1925ab7a76af1a6fb9
solomonaiyede1 <solomonaiyede@gmail.com>
2024-10-22 19:05:27 +0100

    my first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// js/script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
